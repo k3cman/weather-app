20c772655fd2f60d7d938d5aeb67b3c6c1481f9c
Kecman <kecman.dev@gmail.com>
2020-09-20 23:22:42 +0200

    create chart component

--- src/app/shared/components/card/card.module.ts
+++ src/app/shared/components/card/card.module.ts
@@ -7,10 +7,11 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { CardDetailsComponent } from './card-details/card-details.component';
 import { CardClosedComponent } from './card-closed/card-closed.component';
 import { CardFooterComponent } from './card-footer/card-footer.component';
+import { ForecastModule } from '../forecast/forecast.module';
 
 @NgModule({
 	declarations: [CardComponent, CardDetailsComponent, CardClosedComponent, CardFooterComponent],
 	exports: [CardComponent, CardDetailsComponent, CardClosedComponent],
-	imports: [CommonModule, FontAwesomeModule, PipesModule, NgxChartsModule],
+	imports: [CommonModule, FontAwesomeModule, PipesModule, NgxChartsModule, ForecastModule],
 })
 export class CardModule {}
