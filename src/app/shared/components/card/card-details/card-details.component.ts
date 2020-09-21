bc6346c918a955984f5d712b800d76ba7350df21
Kecman <kecman.dev@gmail.com>
2020-09-21 17:39:32 +0200

    remove until destory

--- src/app/shared/components/card/card-details/card-details.component.ts
+++ src/app/shared/components/card/card-details/card-details.component.ts
@@ -1,22 +1,20 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../../core/store/models/app.state';
 import { selectCurrentForecast } from '../../../../core/store/selectors/forecast.selector';
 import { filter } from 'rxjs/operators';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { ForecastType } from '../../../models/enums/forecast-type.enum';
 import { HOUR_BUTTON_OPTIONS } from '../../../consts/hour-button-options';
 import { FORECAST_BUTTON_OPTIONS } from '../../../consts/forecast-button-options';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
-@UntilDestroy()
 @Component({
 	selector: 'card-details',
 	templateUrl: './card-details.component.html',
 	styleUrls: ['./card-details.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CardDetailsComponent implements OnInit {
+export class CardDetailsComponent implements OnInit, OnDestroy {
 	private forecastData$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 	public forecast$ = this.forecastData$.asObservable();
 	public activeForecast: ForecastType = ForecastType.TEMPERATURE;
@@ -25,21 +23,20 @@ export class CardDetailsComponent implements OnInit {
 	public forecastButtons = FORECAST_BUTTON_OPTIONS;
 
 	public hourButtons = HOUR_BUTTON_OPTIONS;
-
+	private subscriptions: Subscription[] = [];
 	@Output() public readonly close: EventEmitter<void> = new EventEmitter<void>();
 
 	constructor(private store: Store<AppState>) {}
 
 	ngOnInit(): void {
-		this.store
-			.select(selectCurrentForecast)
-			.pipe(
-				filter((e) => !!e),
-				untilDestroyed(this)
-			)
-			.subscribe((val) => {
-				this.forecastData$.next(val);
-			});
+		this.subscriptions.push(
+			this.store
+				.select(selectCurrentForecast)
+				.pipe(filter((e) => !!e))
+				.subscribe((val) => {
+					this.forecastData$.next(val);
+				})
+		);
 	}
 
 	public setForecastType(type: ForecastType) {
@@ -48,5 +45,9 @@ export class CardDetailsComponent implements OnInit {
 
 	public changeHours(number: number) {
 		this.numberOfHours = number;
+	}
+
+	public ngOnDestroy() {
+		this.subscriptions.forEach((sub: Subscription) => sub.unsubscribe());
 	}
 }
