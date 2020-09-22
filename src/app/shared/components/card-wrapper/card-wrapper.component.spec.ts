e5674cb05be9525d522ed793399ac5105e7e40ca
Kecman <kecman.dev@gmail.com>
2020-09-22 17:04:59 +0200

    tests

--- src/app/shared/components/card-wrapper/card-wrapper.component.spec.ts
+++ src/app/shared/components/card-wrapper/card-wrapper.component.spec.ts
@@ -1,25 +1,78 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardWrapperComponent } from './card-wrapper.component';
+import { CommonModule } from '@angular/common';
+import { CardModule } from '../card/card.module';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { currentPlaceForTest } from '../../utils/testing';
+import { TimeOfDay } from '../../models/enums/time-of-day.enum';
+import { CardState } from '../../models/enums/card-state.enum';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CardWrapperComponent', () => {
-  let component: CardWrapperComponent;
-  let fixture: ComponentFixture<CardWrapperComponent>;
+	let component: CardWrapperComponent;
+	let fixture: ComponentFixture<CardWrapperComponent>;
+	let store: MockStore;
+	const initialState = {
+		userInterface: {
+			selected: {
+				lat: null,
+				lon: null,
+				id: null,
+			},
+		},
+	};
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [CardWrapperComponent],
+			imports: [CommonModule, CardModule, BrowserAnimationsModule],
+			providers: [provideMockStore({ initialState })],
+		})
+			.overrideComponent(CardWrapperComponent, {
+				set: { changeDetection: ChangeDetectionStrategy.Default },
+			})
+			.compileComponents();
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CardWrapperComponent ]
-    })
-    .compileComponents();
-  }));
+		store = TestBed.inject(MockStore);
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CardWrapperComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(CardWrapperComponent);
+		component = fixture.componentInstance;
+		component.place = currentPlaceForTest;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should display only card', async(() => {
+		fixture.whenStable().then(() => {
+			expect(component.timeOfDay).toEqual(TimeOfDay.DAY);
+			expect(component.currentState).toEqual(CardState.STANDARD);
+			const card = fixture.debugElement.query(By.css('card'));
+			expect(card.nativeElement).toBeTruthy();
+		});
+	}));
+
+	it('should open the card when selected', async(() => {
+		component.cords = { cords: { lat: 48.14, lon: 11.58 }, id: '2867714' };
+		store.setState({
+			userInterface: {
+				selected: {
+					lat: 48.14,
+					lon: 11.58,
+					id: '2867714',
+				},
+			},
+		});
+		fixture.detectChanges();
+
+		fixture.whenStable().then(() => {
+			expect(component.currentState === CardState.EXPANDED);
+		});
+	}));
 });
--- src/app/shared/components/forecast-chart/forecast-chart.component.spec.ts
+++ src/app/shared/components/forecast-chart/forecast-chart.component.spec.ts
@@ -1,15 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ForecastChartComponent } from './forecast-chart.component';
 import { ChangeDetectionStrategy } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { LineChartModule } from '@swimlane/ngx-charts';
+import { TemperaturePipe } from '../../../core/pipes/temperature/temperature.pipe';
+import { ForecastType } from '../../models/enums/forecast-type.enum';
+import { hourlyDataForTests } from '../../utils/testing';
+import { filter } from 'rxjs/operators';
+import { By } from '@angular/platform-browser';
 
-describe('ForecastComponent', () => {
+describe('ForecastChartComponent', () => {
 	let component: ForecastChartComponent;
 	let fixture: ComponentFixture<ForecastChartComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [ForecastChartComponent],
+			imports: [BrowserAnimationsModule, CommonModule, LineChartModule],
+			providers: [TemperaturePipe],
 		})
 			.overrideComponent(ForecastChartComponent, {
 				set: { changeDetection: ChangeDetectionStrategy.Default },
@@ -20,6 +29,11 @@ describe('ForecastComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(ForecastChartComponent);
 		component = fixture.componentInstance;
+		component.data = {
+			hourly: hourlyDataForTests,
+		};
+		component.forecastType = ForecastType.TEMPERATURE;
+		component.selectedPeriod = 12;
 		fixture.detectChanges();
 	});
 
@@ -27,5 +41,16 @@ describe('ForecastComponent', () => {
 		expect(component).toBeTruthy();
 	});
 
-	it('should set');
+	it('Should set data with Inputs and render chart', async(() => {
+		fixture.whenStable().then(() => {
+			expect((component as any)._data).toEqual({ hourly: hourlyDataForTests });
+			expect(component.numberOfHours).toEqual(12);
+			component.chartData$.pipe(filter((e) => !!e)).subscribe((val) => {
+				expect(val.temperature[0].series.length).toEqual(12);
+				expect(val.wind[0].series.length).toEqual(12);
+			});
+			const chart = fixture.debugElement.query(By.css('ngx-charts-line-chart'));
+			expect(chart.nativeElement).toBeTruthy();
+		});
+	}));
 });
